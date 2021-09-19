daba74bfe94bb46670e879ec58c2e3c6680f2c9e
Heylon <heylon96@hotmail.com>
2021-09-19 17:14:39 +1000

    Initial commit

---
+++ Automated-AI-Video-Processing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Automated_AI_Video_Processing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
