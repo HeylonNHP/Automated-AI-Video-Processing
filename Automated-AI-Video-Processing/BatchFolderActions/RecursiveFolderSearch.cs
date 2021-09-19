3010db42aacb461bffdeff82bcde50d9f22ce314
Heylon <heylon96@hotmail.com>
2021-09-19 22:14:21 +1000

    Add RecursiveFolderSearch.cs

---
+++ Automated-AI-Video-Processing/BatchFolderActions/RecursiveFolderSearch.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Automated_AI_Video_Processing.BatchFolderActions
+{
+    public static class RecursiveFolderSearch
+    {
+        public static IEnumerable<string> Search(string path)
+        {
+            return Directory.EnumerateFiles(path, "*", new EnumerationOptions
+            {
+                IgnoreInaccessible = true,
+                RecurseSubdirectories = true
+            });
+        }
+    }
+}
