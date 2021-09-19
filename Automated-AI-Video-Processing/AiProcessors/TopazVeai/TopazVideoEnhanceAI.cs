129b91321c1b0743f6f31aa5d7176fa0cbd09683
Heylon <heylon96@hotmail.com>
2021-09-19 19:09:04 +1000

    Demo code for TopazVideoEnhanceAI.cs

--- Automated-AI-Video-Processing/AiProcessors/TopazVeai/TopazVideoEnhanceAI.cs
+++ Automated-AI-Video-Processing/AiProcessors/TopazVeai/TopazVideoEnhanceAI.cs
@@ -1,8 +1,12 @@
 ﻿
+using Automated_AI_Video_Processing.ProcessExecution;
+
 namespace Automated_AI_Video_Processing.AiProcessors
 {
     public class TopazVideoEnhanceAI
     {
+        private static string VeaiExeLocation = "C:\\Program Files\\Topaz Labs LLC\\Topaz Video Enhance AI\\veai.exe";
+        
         private string inputFilename;
         private TopazVeaiOutputFormats outputFormat;
         private TopazVeaiModels AiModel;
@@ -19,6 +23,26 @@ namespace Automated_AI_Video_Processing.AiProcessors
             this.AiModel = aiModel;
             this.CudaDevice = cudaDevice;
             this.scalingDetails = scalingDetails;
+        }
+
+        public void runAsync()
+        {
+            string[] arguments = new string[]
+            {
+                "-i",
+                inputFilename,
+                "-f",
+                "mov_proreshq",
+                "-s",
+                "1",
+                "-m",
+                "amq-13",
+                "-c",
+                CudaDevice.ToString()
+            };
+            
+            LaunchProcess process = new LaunchProcess(VeaiExeLocation, arguments);
+            process.run();
         }
     }
 }
