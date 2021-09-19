8bea72f15890a6149ba33011643f3df4f6c0ddbc
Heylon <heylon96@hotmail.com>
2021-09-19 18:52:19 +1000

    Add wait for exit

--- Automated-AI-Video-Processing/ProcessExecution/LaunchProcess.cs
+++ Automated-AI-Video-Processing/ProcessExecution/LaunchProcess.cs
@@ -11,8 +11,8 @@ namespace Automated_AI_Video_Processing.ProcessExecution
         private Process selectedProcess = new Process();
         public DataReceivedEventHandler stdErrOutputEvent;
         public DataReceivedEventHandler stdOutOutputEvent;
-        
-        
+
+
         public LaunchProcess(string filename, string[] args)
         {
             selectedProcess.StartInfo.FileName = filename;
@@ -20,11 +20,11 @@ namespace Automated_AI_Video_Processing.ProcessExecution
 
             selectedProcess.ErrorDataReceived += (object sender, DataReceivedEventArgs e) =>
             {
-                stdErrOutputEvent.Invoke(sender,e);
+                stdErrOutputEvent.Invoke(sender, e);
             };
             selectedProcess.OutputDataReceived += (sender, eventArgs) =>
             {
-                stdOutOutputEvent.Invoke(sender,eventArgs);
+                stdOutOutputEvent.Invoke(sender, eventArgs);
             };
         }
 
@@ -35,63 +35,46 @@ namespace Automated_AI_Video_Processing.ProcessExecution
 
         public ProcessStartInfo getStartInfo
         {
-            get
-            {
-                return selectedProcess.StartInfo;
-            }
+            get { return selectedProcess.StartInfo; }
         }
 
         public StreamWriter stdIn
         {
-            get
-            {
-                return selectedProcess.StandardInput;
-            }
+            get { return selectedProcess.StandardInput; }
         }
 
         public StreamReader stdErr
         {
-            get
-            {
-                return selectedProcess.StandardError;
-            }
+            get { return selectedProcess.StandardError; }
         }
 
         public StreamReader stdOut
         {
-            get
-            {
-                return selectedProcess.StandardOutput;
-            }
+            get { return selectedProcess.StandardOutput; }
         }
 
         public bool redirectStdIn
         {
-            set
-            {
-                selectedProcess.StartInfo.RedirectStandardInput = value;
-            }
+            set { selectedProcess.StartInfo.RedirectStandardInput = value; }
         }
 
         public bool redirectStdErr
         {
-            set
-            {
-                selectedProcess.StartInfo.RedirectStandardError = value;
-            }
+            set { selectedProcess.StartInfo.RedirectStandardError = value; }
         }
 
         public bool redirectStdOut
         {
-            set
-            {
-                selectedProcess.StartInfo.RedirectStandardOutput = value;
-            }
+            set { selectedProcess.StartInfo.RedirectStandardOutput = value; }
         }
 
-        public void run()
+        public void run(bool waitForExit = true)
         {
             selectedProcess.Start();
+            if (waitForExit)
+            {
+                selectedProcess.WaitForExit();
+            }
         }
     }
 }
