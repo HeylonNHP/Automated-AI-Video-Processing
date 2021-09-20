b348a21544651d01be5f6fc882d4bb9f05e17279
Heylon <heylon96@hotmail.com>
2021-09-20 19:45:06 +1000

    Create CliMenuItem.cs

---
+++ Automated-AI-Video-Processing/UserInterface/CliMenuItem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Automated_AI_Video_Processing.UserInterface
+{
+    public class CliMenuItem
+    {
+        private string captionInternal;
+        private Action runActionInternal;
+        
+        public CliMenuItem(string caption, Action runAction)
+        {
+            this.captionInternal = caption;
+            this.runActionInternal = runAction;
+        }
+
+        public string caption
+        {
+            get
+            {
+                return captionInternal;
+            }
+        }
+
+        public Action runAction
+        {
+            get
+            {
+                return runActionInternal;
+            }
+        }
+
+        public void executeAction()
+        {
+            runActionInternal();
+        }
+    }
+}
