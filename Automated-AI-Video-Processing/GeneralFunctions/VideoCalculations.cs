835fdee1b03462c37451ac5dcbdc071328762077
Heylon <heylon96@hotmail.com>
2021-09-20 19:01:12 +1000

    Create video scaling calculation function

---
+++ Automated-AI-Video-Processing/GeneralFunctions/VideoCalculations.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Drawing;
+
+namespace Automated_AI_Video_Processing.GeneralFunctions
+{
+    public static class VideoCalculations
+    {
+        public static Point scaleToDesiredResHeightMOD2(Point resolution, int desiredHeight)
+        {
+            Func<int,bool> isMod2 = (int dimension) =>
+            {
+                return dimension % 2 == 0;
+            };
+
+            if (!isMod2(desiredHeight))
+            {
+                throw new Exception("Height not MOD2");
+            }
+            
+            float scaleFactor = (float)desiredHeight / resolution.Y;
+
+            int outWidth = (int)Math.Floor(resolution.X * scaleFactor);
+
+            if (isMod2(outWidth))
+            {
+                return new Point(desiredHeight, outWidth);
+            }
+
+            outWidth = (int)Math.Ceiling(resolution.X * scaleFactor);
+
+            if (isMod2(outWidth))
+            {
+                return new Point(desiredHeight, outWidth);
+            }
+
+            return new Point(desiredHeight, outWidth + 1);
+        }
+    }
+}
